70841204dc6b211307402277a05fae2aa0648317
Bethany <blatham4@charlotte.edu>
2026-09-18 10:39:02 -0400

    feat: add play game function

--- main.py
+++ main.py
@@ -11,4 +11,21 @@ def give_hint(guess, secret_number):
    else:
        print("Your guess is too high")
 
+#Runs the game and keeps up with attempts and if they get it right
+def play_game():
+   secret_number = random.randint(1,100)
+   attempts= 0
 
+   print("Guess the number between 1 and 100")
+
+   while True:
+       guess=get_guess()
+       attempts+=1
+
+       if guess==secret_number:
+           print(f"You win, you guessed in {attempts} attempts")
+           break
+       give_hint(guess, secret_number)
+
+#calls the play game method
+play_game()
