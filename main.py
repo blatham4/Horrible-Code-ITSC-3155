4ae852b38bc65a384136271eeda2c7f6fd0d5347
Bethany <blatham4@charlotte.edu>
2026-09-18 10:38:14 -0400

    feat: add give hint function

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import random
 #Gets the number that the user guesses
 def get_guess():
    return int(input("Enter your guess (1-100)"))
+
+#gives the player a hint so they can make a guess
+def give_hint(guess, secret_number):
+   if guess<secret_number:
+       print("Your guess is too low")
+   else:
+       print("Your guess is too high")
+
+
